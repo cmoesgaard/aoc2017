3340400668087618b155ff98a66b3b384ca44b5a
Christoffer Moesgaard <christoffer@magenta.dk>
2017-12-05 10:19:25 +0100

    Added solutions for day 1 and 2

---
+++ 1/1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+with open('1.input', 'r') as f:
+    input_string = f.readline().rstrip()
+
+
+def part_one():
+    sum = 0
+    for index, char in enumerate(input_string):
+        next_index = (index + 1) % (len(input_string))
+        next_char = input_string[next_index]
+        if char == next_char:
+            sum += int(char)
+    return sum
+
+
+def part_two():
+    sum = 0
+    input_length = len(input_string)
+    for index, char in enumerate(input_string):
+        next_index = (index + int(input_length / 2)) % (len(input_string))
+        next_char = input_string[next_index]
+        if char == next_char:
+            sum += int(char)
+    return sum
+
+
+print("Part 1: {}".format(part_one()))
+print("Part 2: {}".format(part_two()))
---
+++ 2/2.py
@@ -0,0 +1,36 @@
+import csv
+
+import functools
+
+import itertools
+
+
+def get_difference(string_list):
+    sorted_numbers = sorted(map(int, string_list))
+    return sorted_numbers[-1] - sorted_numbers[0]
+
+
+def get_evenly_divisible(string_list):
+    sorted_numbers = reversed(sorted(map(int, string_list)))
+    for number in itertools.combinations(sorted_numbers, 2):
+        modulo = number[0] % number[1]
+        if modulo == 0:
+            return int(number[0] / number[1])
+
+
+def part_one():
+    with open('2.input', 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        return functools.reduce(lambda s, x: s + get_difference(x),
+                                reader, 0)
+
+
+def part_two():
+    with open('2.input', 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        return functools.reduce(lambda s, x: s + get_evenly_divisible(x),
+                                reader, 0)
+
+
+print("Part 1: {}".format(part_one()))
+print("Part 2: {}".format(part_two()))
