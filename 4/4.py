0c3ddd9228b3e51ab6f65945f9a34ba8d605338c
Christoffer Moesgaard <cmoesgaard@gmail.com>
2017-12-11 18:01:22 +0100

    Cool, functional and only semi-unreadable solution for day 4

---
+++ 4/4.py
@@ -0,0 +1,15 @@
+def part_one():
+    with open('4.input', 'r') as f:
+        words = map(lambda x: x.split(), f)
+        return len(list(filter(lambda x: len(x) == len(set(x)), words)))
+
+
+def part_two():
+    with open('4.input', 'r') as f:
+        sorted_words = map(lambda x: list(map(lambda y: "".join(sorted(y)), x.split())), f)
+        valid_phrases = filter(lambda x: len(x) == len(set(x)), sorted_words)
+        return len(list(valid_phrases))
+
+
+print("Part 1: {}".format(part_one()))
+print("Part 2: {}".format(part_two()))
